3bf2a220ff9bbd8be4f5a1bb2b606220b1dc26a8
Norbert Frank Mba <norbertmbafrank@gmail.com>
2022-11-26 18:02:07 +0100

    update dapp with smart contract and abi

---
+++ Artifacts/abi/abiManager.ts
@@ -0,0 +1,12 @@
+import nftABI from "./nftAbi.json"
+import accessControlABI from "./accessControlAbi.json"
+import auctionABI from "./auctionAbi.json"
+import addressManagerABI from "./addressManagerAbi.json"
+import villageSquareABI from "./villageSquareAbi.json"
+
+export const nftAbi = nftABI
+export const accessControlAbi = accessControlABI
+export const auctionAbi = auctionABI
+export const addressManagerAbi = addressManagerABI
+export const villageSquareAbi = villageSquareABI
+
---
+++ Artifacts/contractAddress/contractManager.ts
@@ -0,0 +1,9 @@
+export const nftAddress = "0x2Cdb125180A315afd2926850b56616AdADBa840B"
+
+export const auctionAddress = "0x5d3648C8088cD3951F71Ce2c31ffaba43ba1aBFB"
+
+export const villageSquare = "0xf79DbBe121E52255aBdC884F7D3E47C8D1071ee5"
+
+export const accessControls = "0x415CF6D96270e5252dc7Ba35De2525769EB73490"
+
+export const addressManagerAddress = "0x4bc0DABe093ed823F200baEfC039d055dE3bCF29 "
