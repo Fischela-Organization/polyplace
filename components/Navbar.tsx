cfc99fcbcf1438e998d3facdb55b1c6dc17ac815
Norbert Frank Mba <norbertmbafrank@gmail.com>
2022-11-27 16:54:30 +0100

    add mint nft functionality

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,35 +1,51 @@
-import { useState, useEffect, useContext } from 'react';
+import { useState, useEffect, useContext } from "react";
 
-import { useRouter } from 'next/router';
-import { useTheme } from 'next-themes';
-import Image from 'next/image';
-import Link from 'next/link';
+import { useRouter } from "next/router";
+import { useTheme } from "next-themes";
+import Image from "next/image";
+import Link from "next/link";
 
+import images from "../assets";
 
-import images from '../assets';
+import Button from "./Button";
+import { ConnectButton } from "@web3uikit/web3";
+import { useMoralis } from "react-moralis";
 
-import Button from './Button';
-import { ConnectButton } from '@web3uikit/web3';
-
-interface MenuItemsInterface{
-  isMobile?: any, active?: any, setActive?: any, setIsOpen?: any
+interface MenuItemsInterface {
+  isMobile?: any;
+  active?: any;
+  setActive?: any;
+  setIsOpen?: any;
 }
 
-const MenuItems = ({ isMobile, active, setActive, setIsOpen }: MenuItemsInterface) => {
+const MenuItems = ({
+  isMobile,
+  active,
+  setActive,
+  setIsOpen,
+}: MenuItemsInterface) => {
   const generateLink = (i: number) => {
     switch (i) {
-      case 0: return '/';
-      case 1: return '/listed-nfts';
-      case 2: return '/my-nfts';
-      case 3: return '/dispute-lists';
+      case 0:
+        return "/";
+      case 1:
+        return "/listed-nfts";
+      case 2:
+        return "/my-nfts";
+      case 3:
+        return "/dispute-lists";
       default:
-        return ''
+        return "";
     }
   };
 
   return (
-    <ul className={`list-none flexCenter flex-row ${isMobile && 'flex-col h-full'}`}>
-      {['Explore NFTs', 'Listed NFTs', 'My NFTs', "Disputes"].map((item, i) => (
+    <ul
+      className={`list-none flexCenter flex-row ${
+        isMobile && "flex-col h-full"
+      }`}
+    >
+      {["Explore NFTs", "Listed NFTs", "My NFTs", "Disputes"].map((item, i) => (
         <li
           key={i}
           onClick={() => {
@@ -38,9 +54,11 @@ const MenuItems = ({ isMobile, active, setActive, setIsOpen }: MenuItemsInterfac
             if (isMobile) setIsOpen(false);
           }}
           className={`flex flex-row items-center font-poppins font-semibold text-base dark:hover:text-white hover:text-nft-dark mx-3 transition duration-300
-        ${active === item
-            ? 'dark:text-white text-nft-black-1'
-            : 'dark:text-nft-gray-3 text-nft-gray-2'}
+        ${
+          active === item
+            ? "dark:text-white text-nft-black-1"
+            : "dark:text-nft-gray-3 text-nft-gray-2"
+        }
          `}
         >
           <Link href={generateLink(i)}>{item}</Link>
@@ -50,64 +68,81 @@ const MenuItems = ({ isMobile, active, setActive, setIsOpen }: MenuItemsInterfac
   );
 };
 
-interface ButtonGroupInterface{
-  setActive: any, router: any, setIsOpen: any
+interface ButtonGroupInterface {
+  setActive: any;
+  router: any;
+  setIsOpen: any;
 }
 
-const ButtonGroup = ({ setActive, router, setIsOpen }: ButtonGroupInterface) => {
-  const { connectWallet, currentAccount } = {connectWallet: "", currentAccount: ""}
+const ButtonGroup = ({
+  setActive,
+  router,
+  setIsOpen,
+}: ButtonGroupInterface) => {
+  const { connectWallet, currentAccount } = {
+    connectWallet: "",
+    currentAccount: "",
+  };
+  const { signup, isAuthenticated, user, account } = useMoralis();
 
-  return currentAccount ? (
-    <Button
-      btnName="Create"
-      classStyles="mx-2 rounded-xl"
-      handleClick={() => {
-        setActive('');
-        setIsOpen(false);
+  useEffect(() => {
+    console.log(user, "USER", account);
+  });
 
-        router.push('/create-nft');
-      }}
-    />
+  return account ? (
+    <div style={{display: "flex"}}>
+      <Button
+        btnName="Create"
+        classStyles="mx-2 rounded-xl"
+        handleClick={() => {
+          setActive("");
+          setIsOpen(false);
+
+          router.push("/create-nft");
+        }}
+      />
+      <ConnectButton />
+    </div>
   ) : (
+    
     // <Button
     //   btnName="Connect"
     //   classStyles="mx-2 rounded-xl"
     //   handleClick={connectWallet}
     // />
 
-    <ConnectButton/>
+    <ConnectButton />
   );
 };
 
-
 const checkActive = (active: any, setActive: any, router: any) => {
   switch (router.pathname) {
-    case '/':
-      if (active !== 'Explore NFTs') setActive('Explore NFTs');
+    case "/":
+      if (active !== "Explore NFTs") setActive("Explore NFTs");
       break;
-    case '/listed-nfts':
-      if (active !== 'Listed NFTs') setActive('Listed NFTs');
+    case "/listed-nfts":
+      if (active !== "Listed NFTs") setActive("Listed NFTs");
       break;
-    case '/my-nfts':
-      if (active !== 'My NFTs') setActive('My NFTs');
+    case "/my-nfts":
+      if (active !== "My NFTs") setActive("My NFTs");
       break;
-    case '/create-nft':
-      setActive('');
+    case "/create-nft":
+      setActive("");
       break;
 
     default:
-      setActive('');
+      setActive("");
   }
 };
 
 const Navbar = () => {
   const { theme, setTheme } = useTheme();
   const router = useRouter();
-  const [active, setActive] = useState('Explore NFTs');
+  const [active, setActive] = useState("Explore NFTs");
   const [isOpen, setIsOpen] = useState(false);
 
   useEffect(() => {
-    setTheme('dark');
+    setTheme("dark");
   }, []);
 
   useEffect(() => {
@@ -121,22 +156,36 @@ const Navbar = () => {
           <div
             className="flexCenter md:hidden cursor-pointer"
             onClick={() => {
-              setActive('Explore NFTs');
+              setActive("Explore NFTs");
             }}
           >
-            <Image src={images.logo02} objectFit="contain" width={40} height={40} alt="logo" />
-            <p className="dark:text-white text-nft-black-1 font-bold text-xl ml-1">RealIncom</p>
+            <Image
+              src={images.logo02}
+              objectFit="contain"
+              width={40}
+              height={40}
+              alt="logo"
+            />
+            <p className="dark:text-white text-nft-black-1 font-bold text-xl ml-1">
+              RealIncom
+            </p>
           </div>
         </Link>
         <Link href="/">
           <div
             className="hidden md:flex cursor-pointer"
             onClick={() => {
-              setActive('Explore NFTs');
+              setActive("Explore NFTs");
               setIsOpen(false);
             }}
           >
-            <Image src={images.logo02} objectFit="contain" width={40} height={40} alt="logomobile" />
+            <Image
+              src={images.logo02}
+              objectFit="contain"
+              width={40}
+              height={40}
+              alt="logomobile"
+            />
           </div>
         </Link>
       </div>
@@ -146,9 +195,12 @@ const Navbar = () => {
             type="checkbox"
             className="checkbox"
             id="checkbox"
-            onChange={() => setTheme(theme === 'light' ? 'dark' : 'light')}
+            onChange={() => setTheme(theme === "light" ? "dark" : "light")}
           />
-          <label htmlFor="checkbox" className="flexBetween w-8 h-4 bg-black rounded-2xl p-1 relative label cursor-pointer">
+          <label
+            htmlFor="checkbox"
+            className="flexBetween w-8 h-4 bg-black rounded-2xl p-1 relative label cursor-pointer"
+          >
             <i className="fas fa-moon" />
             <i className="fas fa-sun" />
             <div className="w-3 h-3 absolute bg-white rounded-full ball" />
@@ -158,47 +210,58 @@ const Navbar = () => {
         <div className="md:hidden flex">
           <MenuItems active={active} setActive={setActive} />
           <div className="ml-3">
-            <ButtonGroup setActive={setActive} router={router} setIsOpen={setIsOpen} />
+            <ButtonGroup
+              setActive={setActive}
+              router={router}
+              setIsOpen={setIsOpen}
+            />
           </div>
         </div>
       </div>
 
       <div className="hidden md:flex ml-2 cursor-pointer">
-        {isOpen
-          ? (
-            <Image
-              src={images.cross}
-              objectFit="contain"
-              width={20}
-              height={20}
-              alt="close"
-              onClick={() => setIsOpen(false)}
-              className={theme === 'light' ? 'filter invert' : ''}
-            />
-          ) : (
-            <Image
-              src={images.menu}
-              objectFit="contain"
-              width={25}
-              height={25}
-              alt="menu"
-              onClick={() => setIsOpen(true)}
-              className={theme === 'light' ? 'filter invert' : ''}
-            />
-          )}
+        {isOpen ? (
+          <Image
+            src={images.cross}
+            objectFit="contain"
+            width={20}
+            height={20}
+            alt="close"
+            onClick={() => setIsOpen(false)}
+            className={theme === "light" ? "filter invert" : ""}
+          />
+        ) : (
+          <Image
+            src={images.menu}
+            objectFit="contain"
+            width={25}
+            height={25}
+            alt="menu"
+            onClick={() => setIsOpen(true)}
+            className={theme === "light" ? "filter invert" : ""}
+          />
+        )}
 
         {isOpen && (
           <div className="fixed inset-0 top-65 dark:bg-nft-dark bg-white z-10 nav-h flex justify-between flex-col">
             <div className="flex-1 p-4">
-              <MenuItems active={active} setActive={setActive} isMobile setIsOpen={setIsOpen} />
+              <MenuItems
+                active={active}
+                setActive={setActive}
+                isMobile
+                setIsOpen={setIsOpen}
+              />
             </div>
             <div className="p-4 border-t dark:border-nft-black-1 border-nft-gray-1 flex justify-center">
-              <ButtonGroup setActive={setActive} router={router} setIsOpen={setIsOpen} />
+              <ButtonGroup
+                setActive={setActive}
+                router={router}
+                setIsOpen={setIsOpen}
+              />
             </div>
           </div>
         )}
       </div>
-
     </nav>
   );
 };
--- pages/create-nft.tsx
+++ pages/create-nft.tsx
@@ -10,15 +10,31 @@ import withTransition from '../components/withTransition';
 import { Button, Input, Loader } from '../components';
 
 import images from '../assets';
+import { useMoralis, useWeb3Contract } from 'react-moralis';
+import { nftAbi } from '../Artifacts/abi/abiManager';
+import { nftAddress } from '../Artifacts/contractAddress/contractManager';
 
 const CreateNFT = () => {
   const [fileUrl, setFileUrl] = useState(null);
   const [formInput, setFormInput] = useState({ price: '', name: '', description: '' });
   const { theme } = useTheme();
-
+  const { signup, isAuthenticated, user, account } = useMoralis();
+  const { data, error, runContractFunction, isFetching, isLoading } = useWeb3Contract({
+    abi: nftAbi,
+    contractAddress: nftAddress,
+    functionName: "mintNFT",
+    params: {
+      _title: "0x2Cdb125180A315afd2926850b56616AdADBa840B",
+      _description : "Hello",
+      _digiURI: "ipfs://"
+    },
+  });
   const uploadToIPFS = (file: any) => null
   const { isLoadingNFT } = {isLoadingNFT:''}
-  const createNFT = (item1: any, item2: any, item3: any) => null
+  const mintNft = async (item1: any, item2: any, item3: any) => {
+   await runContractFunction()
+   console.log(data, account, "DATA")
+  }
   const router = useRouter();
 
   const onDrop = useCallback(async (acceptedFile: any) => {
@@ -56,7 +72,7 @@ const CreateNFT = () => {
           Create a new NFT
         </h1>
         <div className="mt-16">
-          <p className="font-poppins dark:text-white text-nft-black-1 font-semibold text-xl">Upload File</p>
+          <p className="font-poppins dark:text-white text-nft-black-1 font-semibold text-xl">Upload Image</p>
           <div className="mt-4">
             <div {...getRootProps()} className={fileStyle}>
               <input {...getInputProps()} />
@@ -91,6 +107,43 @@ const CreateNFT = () => {
             )}
           </div>
         </div>
+
+        <div className="mt-16">
+          <p className="font-poppins dark:text-white text-nft-black-1 font-semibold text-xl">Upload Doc</p>
+          <div className="mt-4">
+            <div {...getRootProps()} className={fileStyle}>
+              <input {...getInputProps()} />
+              <div className="flexCenter flex-col text-center">
+                <p className="font-poppins dark:text-white text-nft-black-1 font-semibold text-xl">
+                  PDF, DOCX
+                </p>
+                <div className="my-12 w-full flex justify-center">
+                  <Image
+                    src={images.upload}
+                    width={100}
+                    height={100}
+                    objectFit="contain"
+                    alt="file upload"
+                    className={theme === 'light' ? 'filter invert' : ''}
+                  />
+                </div>
+                <p className="font-poppins dark:text-white text-nft-black-1 font-semibold text-sm">
+                  Drag and Drop File,
+                </p>
+                <p className="font-poppins dark:text-white text-nft-black-1 font-semibold text-sm mt-2">
+                  or Browse file on your device.
+                </p>
+              </div>
+            </div>
+            {fileUrl && (
+              <aside>
+                <div>
+                  <img src={fileUrl} alt="asset_file" />
+                </div>
+              </aside>
+            )}
+          </div>
+        </div>
         <Input
           inputType="input"
           title="Name"
@@ -113,7 +166,7 @@ const CreateNFT = () => {
           <Button
             btnName="Create NFT"
             classStyles="rounded-xl"
-            handleClick={() => createNFT(formInput, fileUrl, router)}
+            handleClick={() => mintNft(formInput, fileUrl, router)}
           />
         </div>
       </div>
